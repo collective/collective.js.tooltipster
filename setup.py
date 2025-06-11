7cc6595f5ed70d9c7778258e76072d921249e222
Chris Adam <chris.adam@imio.be>
2025-06-11 12:04:51 +0200

    Changed python version from 3.12 to 3.13

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
         "License :: OSI Approved :: GNU General Public License (GPL)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
     ],
     keywords="Python Plone",
     author="Gauthier BASTIEN",
