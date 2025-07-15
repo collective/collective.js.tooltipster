e5d74dc463fdcd94b0f5f7fe265a335d537ea82d
Chris Adam <chris.adam@imio.be>
2025-07-15 10:18:21 +0200

    Black + isort + zpretty

--- src/collective/js/tooltipster/tests/test_setup.py
+++ src/collective/js/tooltipster/tests/test_setup.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 """Setup tests for this package."""
-from collective.js.tooltipster.testing import COLLECTIVE_JS_TOOLTIPSTER_INTEGRATION_TESTING  # noqa
+from collective.js.tooltipster.testing import (
+    COLLECTIVE_JS_TOOLTIPSTER_INTEGRATION_TESTING,
+)  # noqa
 from plone import api
 
 import unittest2 as unittest
@@ -13,20 +15,21 @@ class TestSetup(unittest.TestCase):
 
     def setUp(self):
         """Custom shared utility setup for tests."""
-        self.portal = self.layer['portal']
-        self.installer = api.portal.get_tool('portal_quickinstaller')
+        self.portal = self.layer["portal"]
+        self.installer = api.portal.get_tool("portal_quickinstaller")
 
     def test_product_installed(self):
         """Test if collective.js.tooltipster is installed with portal_quickinstaller."""
-        self.assertTrue(self.installer.isProductInstalled('collective.js.tooltipster'))
+        self.assertTrue(self.installer.isProductInstalled("collective.js.tooltipster"))
 
     def test_uninstall(self):
         """Test if collective.js.tooltipster is cleanly uninstalled."""
-        self.installer.uninstallProducts(['collective.js.tooltipster'])
-        self.assertFalse(self.installer.isProductInstalled('collective.js.tooltipster'))
+        self.installer.uninstallProducts(["collective.js.tooltipster"])
+        self.assertFalse(self.installer.isProductInstalled("collective.js.tooltipster"))
 
     def test_browserlayer(self):
         """Test that ICollectiveJsTooltipsterLayer is registered."""
         from collective.js.tooltipster.interfaces import ICollectiveJsTooltipsterLayer
         from plone.browserlayer import utils
+
         self.assertIn(ICollectiveJsTooltipsterLayer, utils.registered_layers())
--- src/collective/js/tooltipster/upgrades.py
+++ src/collective/js/tooltipster/upgrades.py
@@ -4,6 +4,9 @@ from plone import api
 
 def upgrade_to_2000(context):
     """Reapply 'themes' profile if necessary."""
-    csstool = api.portal.get_tool('portal_css')
-    if '++resource++collective.js.tooltipster/tooltipster-noir.css' in csstool.concatenatedresources:
-        context.runAllImportStepsFromProfile('collective.js.tooltipster:themes')
+    csstool = api.portal.get_tool("portal_css")
+    if (
+        "++resource++collective.js.tooltipster/tooltipster-noir.css"
+        in csstool.concatenatedresources
+    ):
+        context.runAllImportStepsFromProfile("collective.js.tooltipster:themes")
