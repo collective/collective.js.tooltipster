2259fdedf45f1906ffb4855a2495b83e2a364169
Chris Adam <chris.adam@imio.be>
2025-07-15 10:18:21 +0200

    Black + isort + zpretty

--- src/collective/js/tooltipster/testing.py
+++ src/collective/js/tooltipster/testing.py
@@ -10,22 +10,22 @@ import collective.js.tooltipster
 
 COLLECTIVE_JS_TOOLTIPSTER_FIXTURE = PloneWithPackageLayer(
     zcml_package=collective.js.tooltipster,
-    zcml_filename='testing.zcml',
-    gs_profile_id='collective.js.tooltipster:testing',
-    name='CollectiveJsTooltipsterLayer',
-    additional_z2_products=()
+    zcml_filename="testing.zcml",
+    gs_profile_id="collective.js.tooltipster:testing",
+    name="CollectiveJsTooltipsterLayer",
+    additional_z2_products=(),
 )
 
 
 COLLECTIVE_JS_TOOLTIPSTER_INTEGRATION_TESTING = IntegrationTesting(
     bases=(COLLECTIVE_JS_TOOLTIPSTER_FIXTURE,),
-    name='CollectiveJsTooltipsterLayer:IntegrationTesting'
+    name="CollectiveJsTooltipsterLayer:IntegrationTesting",
 )
 
 
 COLLECTIVE_JS_TOOLTIPSTER_FUNCTIONAL_TESTING = FunctionalTesting(
     bases=(COLLECTIVE_JS_TOOLTIPSTER_FIXTURE,),
-    name='CollectiveJsTooltipsterLayer:FunctionalTesting'
+    name="CollectiveJsTooltipsterLayer:FunctionalTesting",
 )
 
 
@@ -33,7 +33,7 @@ COLLECTIVE_JS_TOOLTIPSTER_ACCEPTANCE_TESTING = FunctionalTesting(
     bases=(
         COLLECTIVE_JS_TOOLTIPSTER_FIXTURE,
         REMOTE_LIBRARY_BUNDLE_FIXTURE,
-        z2.ZSERVER_FIXTURE
+        z2.ZSERVER_FIXTURE,
     ),
-    name='CollectiveJsTooltipsterLayer:AcceptanceTesting'
+    name="CollectiveJsTooltipsterLayer:AcceptanceTesting",
 )
